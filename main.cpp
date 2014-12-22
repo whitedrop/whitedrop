8fea31e56e4e161a56790c1ee7526cc04877e45a
vinz243 <vinz243>
2014-12-22 12:30:54 +0100

    Add JSON Parsing

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 #include <windows.h>
+#include "./parser/include/parser.h"
 #include "engine/lib/Whitedrop.h"
 int WINAPI WinMain ( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd )
 {
+	Parser::input("../media/maps/basic-map.json");
+	Parser::World world = Parser::load();
+	
 	if(Whitedrop::init())
 	{
-		Whitedrop::spawnEntity("cube.mesh", "cube01", Whitedrop::Vector3(10, 10, 10), Whitedrop::Vector3(10, 10, 10));
-		Whitedrop::spawnEntity("cube.mesh", "cube02", Whitedrop::Vector3(20, 10, 10), Whitedrop::Vector3(10, 10, 10));
-		Whitedrop::spawnEntity("cube.mesh", "cube03", Whitedrop::Vector3(10, 50, 10), Whitedrop::Vector3(10, 10, 10));
-
-		Whitedrop::spawnEntity("ogrehead.mesh", "ogre01", Whitedrop::Vector3(50, 50, 50), Whitedrop::Vector3(10, 10, 10));
+		for(int i=0; i < world.entities.size(); i++){
+ 		  
+			Whitedrop::spawnEntity(world.entities[i].mesh, world.entities[i].id, Whitedrop::Vector3(world.entities[i].position.x, world.entities[i].position.y, world.entities[i].position.y), Whitedrop::Vector3(world.entities[i].dimensions.x, world.entities[i].dimensions.y, world.entities[i].dimensions.z));
+		}		
+		
 		Whitedrop::run();
 
 	}
