7525cec81de83862de721c620c0b59fa8bfd9f9c
vinz243 <vinz243>
2014-12-23 23:21:49 +0100

    Add material

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <windows.h>
 #include "./parser/include/parser.h"
 #include "engine/lib/Whitedrop.h"
+#include <string>
 int WINAPI WinMain ( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd )
 {
 	Parser::input("../media/maps/basic-map.json");
@@ -9,8 +10,12 @@ int WINAPI WinMain ( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	if(Whitedrop::init())
 	{
 		for(int i=0; i < world.entities.size(); i++){
- 		  
-			Whitedrop::spawnEntity(world.entities[i].mesh, world.entities[i].id, Whitedrop::Vector3(world.entities[i].position.x, world.entities[i].position.y, world.entities[i].position.y), Whitedrop::Vector3(world.entities[i].dimensions.x, world.entities[i].dimensions.y, world.entities[i].dimensions.z));
+ 		  	std::string material = world.entities[i].material;
+ 		  	std::string mesh = world.entities[i].mesh;
+ 		  	std::string id = world.entities[i].id;
+ 		  	Whitedrop::Vector3 pos = Whitedrop::Vector3(world.entities[i].position.x, world.entities[i].position.y, world.entities[i].position.y);
+ 		  	Whitedrop::Vector3 dim = Whitedrop::Vector3(world.entities[i].dimensions.x, world.entities[i].dimensions.y, world.entities[i].dimensions.z);
+			Whitedrop::spawnEntity(mesh, id, pos, dim, material);
 		}		
 		
 		Whitedrop::run();
